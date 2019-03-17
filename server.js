9605dce14a7aae76b2a578e277b411d267fec2a0
jentino <jentino@gmail.com>
2019-03-17 15:19:11 +0000

    halalaa

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
-const PORT = 80
+const PORT = 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
