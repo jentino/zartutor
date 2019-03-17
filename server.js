51b5d10b1849b45f8ead5f07d8b18c0644d2c981
jentino <jentino@gmail.com>
2019-03-17 15:58:30 +0000

    committtiingg

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const PORT = process.env.PORT || 3000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.send('coo'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash  
+git add -A 
+# read -p "Commit description: " commitiing 
+# git commit -m "$desc"  
+git commit -m "committtiingg"  
+git push origin master
