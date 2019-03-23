e8fd92a723606be42d7e030b2042af5c096c7ccf
jentino <jentino@gmail.com>
2019-03-23 15:56:01 +0000

    committtiingg

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
-const chatmessages = require('./chatmessages/chatmessages.service.js');
+// const chatmessages = require('./chatmessages/chatmessages.service.js');
 // const messages = require('./messages/messages.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
-  app.configure(chatmessages);
+//   app.configure(chatmessages);
 };
