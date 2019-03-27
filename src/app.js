e8bedf139d49fbf1920db44110c944ad429a2585
jentino <jentino@gmail.com>
2019-03-27 15:12:54 +0000

    committtiingg

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ const services = require('./services');
 const appHooks = require('./app.hooks');
 const channels = require('./channels');
 
+const authentication = require('./authentication');
+
+const mongodb = require('./mongodb');
+
 const app = express(feathers());
 
 // Load app configuration
@@ -35,8 +39,10 @@ app.use('/', express.static(app.get('public')));
 app.configure(express.rest());
 
 app.configure(primus({ transformer: 'websockets' }));
+app.configure(mongodb);
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
+app.configure(authentication);
 // Set up our services (see `services/index.js`)
 app.configure(services);
 // Set up event channels (see channels.js)
