ca749eb5e797e348b8db8b6b95ec5790d510609d
jentino <jentino@gmail.com>
2019-03-27 12:45:32 +0000

    committtiingg

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
-const rooms = require('./rooms/rooms.service.js');
-const users = require('./users/users.service.js');
+// const rooms = require('./rooms/rooms.service.js');
+// const users = require('./users/users.service.js');
 // const chatmessages = require('./chatmessages/chatmessages.service.js');
 // const messages = require('./messages/messages.service.js');
 // eslint-disable-next-line no-unused-vars
