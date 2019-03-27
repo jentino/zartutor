31b5dacd8d2e13f793ecbe9b7aa49c15aab98cb9
jentino <jentino@gmail.com>
2019-03-27 10:26:27 +0000

    committtiingg

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const rooms = require('./rooms/rooms.service.js');
+const users = require('./users/users.service.js');
 // const chatmessages = require('./chatmessages/chatmessages.service.js');
 // const messages = require('./messages/messages.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(rooms);
+  app.configure(users);
 };
