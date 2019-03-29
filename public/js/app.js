92788278ee211aa830b349e5468b1985fd017816
jentino <jentino@gmail.com>
2019-03-29 03:47:36 +0000

    committtiingg

--- public/js/app.js
+++ public/js/app.js
@@ -1,51 +1,35 @@
-	var i=0;
-	$(document).ready(function(){
-     $('#add_more').on('click', function(){
-      var colorR = Math.floor((Math.random() * 256));
-      var colorG = Math.floor((Math.random() * 256));
-      var colorB = Math.floor((Math.random() * 256));
-      i++;
-      var html ='<div id="append_no_'+i+'" class="animated bounceInLeft">'+
-          '<div class="input-group mt-3">'+
-		  '<div class="input-group-prepend">'+
-		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
-		  '<i class="fas fa-user-graduate"></i></span>'+
-		  '</div>'+
-		  '<input type="text" placeholder="Student Name"  class="form-control"/>'+
-		  '</div>'+
-		  '<div class="input-group mt-3">'+
-		  '<div class="input-group-prepend">'+
-		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
-		  '<i class="fas fa-phone-square"></i></span>'+
-		  '</div>'+
-		  '<input type="text" placeholder="Student Phone" class="form-control"/>'+
-		  '</div>'+
-		  '<div class="input-group mt-3">'+
-		  '<div class="input-group-prepend">'+
-		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
-		  '<i class="fas fa-at"></i></span>'+
-		  '</div>'+
-		  '<input type="email" placeholder="Student Email" class="form-control"/>'+
-		  '</div></div>';
+jQuery(function ($) {
 
-	  $('#dynamic_container').append(html);
-	  $('#remove_more').fadeIn(function(){
-	  	 $(this).show();
-	  });
-     });
+    $(".sidebar-dropdown > a").click(function() {
+  $(".sidebar-submenu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .parent()
+      .removeClass("active");
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sidebar-submenu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
 
-     $('#remove_more').on('click', function(){
-         
-         $('#append_no_'+i).removeClass('bounceInLeft').addClass('bounceOutRight')
-            .fadeOut(function(){
-            	$(this).remove();
-            });
-            i--;
-            if(i==0){
-            	$('#remove_more').fadeOut(function(){
-            		$(this).hide()
-            	});;
-            }
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+});
+
+
    
-     });
-	});
+   
+});
